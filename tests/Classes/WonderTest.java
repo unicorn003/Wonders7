09487cd4a124f3957e3c683b8a2cb32c3f1b5899
unicorn03 <valentyna.pronina@nure.ua>
2023-01-26 21:05:04 +0100

    I renewed the test class Wonder, because from last time I added some functions

---
+++ tests/Classes/WonderTest.java
@@ -0,0 +1,64 @@
+package Classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WonderTest {
+
+    @Test
+    void makeTaken() {
+    }
+
+    @Test
+    void addMilitaryToken() {
+    }
+
+    @Test
+    void addAgeCard() {
+    }
+
+    @Test
+    void addTreasure() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void isSide() {
+    }
+
+    @Test
+    void isTaken() {
+    }
+
+    @Test
+    void getLocalStage() {
+    }
+
+    @Test
+    void countMilitaryForce() {
+    }
+
+    @Test
+    void fight() {
+    }
+
+    @Test
+    void getStartingResource() {
+    }
+
+    @Test
+    void getResourceLocalStage() {
+    }
+
+    @Test
+    void nextStage() {
+    }
+
+    @Test
+    void countWinningPoint() {
+    }
+}
