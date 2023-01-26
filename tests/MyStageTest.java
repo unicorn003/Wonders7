1966f6bf4c035df0c88d07b7eed5b1c5fb90780a
unicorn03 <valentyna.pronina@nure.ua>
2023-01-26 17:15:12 +0100

    I am working on testing for be sure, that everything works clear

---
+++ tests/MyStageTest.java
@@ -0,0 +1,38 @@
+import Classes.MyStage;
+import Classes.Resource;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStageTest {
+    Resource wood1 = new Resource("raw", "Wood");
+    Resource wood2 = new Resource("raw", "Wood");
+    Resource glass = new Resource("rare", "Glass");
+    Resource papyrus = new Resource("rare", "Papyrus");
+    ArrayList<Resource> resourcesNeed = new ArrayList<Resource>(List.of(wood1, wood2, glass, papyrus));
+    MyStage stage = new MyStage(2, 10, resourcesNeed);
+
+    @Test
+    void getVictoryPoints() {
+         assertEquals(10, stage.getVictoryPoints());
+    }
+
+    @Test
+    void isBuild() {
+    }
+
+    @Test
+    void isEnoughResources() {
+    }
+
+    @Test
+    void resourcesNeed() {
+    }
+
+    @Test
+    void buildStage() {
+    }
+}
