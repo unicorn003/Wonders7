6881fafd3b7b46d9afc3ad7deb4222d77b96f643
unicorn03 <valentyna.pronina@nure.ua>
2023-01-26 18:40:40 +0100

    This code was wrote by Abderahman to simulate a work of Java FX

--- src/Classes/GUIParser.java
+++ src/Classes/GUIParser.java
@@ -5,10 +5,56 @@ import Interfaces.IActions;
 import java.util.Scanner;
 
 public class GUIParser implements IActions {
+
+    @Override
+    public void init() {
+
+    }
+
     public String askName() {
         System.out.println("What's your name ?");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         return name;
+    }
+
+    @Override
+    public void start(String connectionString) {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void startAge(int age) {
+
+    }
+
+    @Override
+    public void startTurn(int turn) {
+
+    }
+
+    @Override
+    public void doEndOfTurn(int turn) {
+
+    }
+
+    @Override
+    public void doEndOfAge(int age) {
+
+    }
+
+    @Override
+    public void showGameResult() {
+
+    }
+
+    @Override
+    public boolean continueGame() {
+        return false;
     }
 }
--- src/Interfaces/IActions.java
+++ src/Interfaces/IActions.java
@@ -1,7 +1,32 @@
 package Interfaces;
 
 public interface IActions {
+    // Initializes the GUI
+    public void init();
     public String askName();
+    // Starts the GUI with the given stage
+    public void start(String connectionString);
+
+    // Restarts the GUI
+    public void restart();
+
+    // Starts the given age in the game
+    public void startAge(int age);
+
+    // Starts the given turn in the game
+    public void startTurn(int turn);
+
+    // Performs end of turn actions for the given turn
+    public void doEndOfTurn(int turn);
+
+    // Performs end of age actions for the given age
+    public void doEndOfAge(int age);
+
+    // Shows the game result
+    public void showGameResult();
+
+    // Returns a boolean indicating if the game should continue
+    public boolean continueGame();
 
 }
 
