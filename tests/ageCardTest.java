ea8d2293b253277b8177bb3504827030dc08022a
unicorn03 <valentyna.pronina@nure.ua>
2023-01-26 15:53:17 +0100

    I am working on testing for be sure, that everything works clear

---
+++ tests/ageCardTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ageCardTest {
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getMinimumPlayers() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void isDirection() {
+    }
+
+    @Test
+    void getEffects() {
+    }
+
+    @Test
+    void getRequiredResources() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+}
