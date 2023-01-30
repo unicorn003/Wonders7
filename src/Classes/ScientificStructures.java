335e4fec1dea6c53c705dec73efcbaefd21c87ec
unicorn03 <valentyna.pronina@nure.ua>
2023-01-31 00:12:59 +0100

    Comments for Treasure class

--- src/Classes/ScientificStructures.java
+++ src/Classes/ScientificStructures.java
@@ -3,13 +3,13 @@ package Classes;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ScientificStructures extends Effect {
+public class ScientificStructures extends Effect { //special class which inherit effect class for green cards
     public ScientificStructures() {
         super("ScientificStructures", "Scientific Structures", 1);
     }
     public int ReturnGreenPoints(ArrayList<ageCard> cards){
         //int result = 0;
-        int Compass = 0;
+        int Compass = 0; // count number of each type of green cards
         int Wheel = 0;
         int Stone = 0;
         for (ageCard card : cards){
@@ -21,7 +21,7 @@ public class ScientificStructures extends Effect {
                 Stone++;
         }
         int[] greens = {Compass, Wheel, Stone};
-        return  (greens[0] * 7 + greens[0]*greens[0] + greens[1]*greens[1] + greens[2]*greens[2]);
+        return  (greens[0] * 7 + greens[0]*greens[0] + greens[1]*greens[1] + greens[2]*greens[2]); // count green card points with special formula
         //return result;
     }
     public int TestCounter(ArrayList<ageCard> cards){ // tested counter system, because previous method didn't work well
