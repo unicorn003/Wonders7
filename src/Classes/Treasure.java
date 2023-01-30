a7cf90d3e0ded5edc5af665eded3555ad5bb1bf3
unicorn03 <valentyna.pronina@nure.ua>
2023-01-31 00:11:44 +0100

    Comments for Treasure class

--- src/Classes/Treasure.java
+++ src/Classes/Treasure.java
@@ -1,6 +1,6 @@
 package Classes;
 
-public class Treasure extends Effect {
+public class Treasure extends Effect { // inherit effect class as treasure coin
 
     public Treasure (int w){
         super("Treasure","Treasure coin", w);
