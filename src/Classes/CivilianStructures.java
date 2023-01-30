ecb43341d5fa4efaa5f3c0722b62c2044735cdfd
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:33:53 +0100

    Comments for CivilianStructures class

--- src/Classes/CivilianStructures.java
+++ src/Classes/CivilianStructures.java
@@ -1,6 +1,6 @@
 package Classes;
 
-public class CivilianStructures extends Effect {
+public class CivilianStructures extends Effect { //inherit effect class for civilian structures
     public CivilianStructures(int w) {
         super("CivilianStructures", "Civilian Structures", w);
     }
