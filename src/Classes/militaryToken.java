cf56457d3275ef4feae20589f4f4e7d727d8422d
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:55:51 +0100

    Comments for militaryToken class

--- src/Classes/militaryToken.java
+++ src/Classes/militaryToken.java
@@ -1,6 +1,6 @@
 package Classes;
 
-public class militaryToken extends Effect {
+public class militaryToken extends Effect { // inherit class of effect class for red cards
     private int amount;
     public militaryToken(int a){
         super("militaryToken","Military Token",a);
