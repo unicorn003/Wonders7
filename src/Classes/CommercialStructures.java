af6d2a266c82b36d44062040192269009fc2562f
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:32:55 +0100

    Comments for CommercialStructures class

--- src/Classes/CommercialStructures.java
+++ src/Classes/CommercialStructures.java
@@ -2,18 +2,18 @@ package Classes;
 
 import java.util.ArrayList;
 
-public class CommercialStructures extends Effect{
+public class CommercialStructures extends Effect{ //implementation of effect class for commercial structures
     public CommercialStructures(int w, String c) {
         super("CommercialStructures", "Commercial Structures", w);
         this.category = c;
     }
-    private String category;
+    private String category; //add category characteristics
     public int CountCategoryPoints(ArrayList<ageCard> cards){
         int k = 0;
-        for (ageCard card : cards){
+        for (ageCard card : cards){ //count the number of cards of given category
             if(card.getCategory().equals(category) )
                 k++;
         }
-        return this.getWinningPoints() * k;
+        return this.getWinningPoints() * k; //multiple number of cards by number of points per card
     }
 }
