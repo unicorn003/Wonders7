6d5612bd0a6d74303dff16915b14086a26507698
unicorn03 <valentyna.pronina@nure.ua>
2023-01-31 00:24:28 +0100

    Comments for Wonder and ScientificStructures classes

--- src/Classes/Wonder.java
+++ src/Classes/Wonder.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Wonder {
+public class Wonder { // main game class which represent an idea of 7 wonders
     private String name;
-    private boolean isTaken;
-    private boolean side;
-    private MyStage localStage;
-    private Resource startingResource;
-    private List<Resource> availableResources = new ArrayList<>();
-    private MyStage stage1;
-    private MyStage stage2;
-    private MyStage stage3;
+    private boolean isTaken; // is someone take this wonder or not
+    private boolean side; // they are two sides of wonder
+    private MyStage localStage; // local state of game
+    private Resource startingResource; // resource which wonder has from  the start
+    private List<Resource> availableResources = new ArrayList<>(); //resource which wonder has
+    private MyStage stage1; // 1 stage of game
+    private MyStage stage2; // 2 stage of game
+    private MyStage stage3; // 3 stage of game
     public Wonder(String n, boolean s, Resource r, MyStage s1, MyStage s2, MyStage s3){
        this.availableResources.add(r);
         this.localStage = s1;
@@ -28,21 +28,21 @@ public class Wonder {
 
     public void MakeTaken(){
         isTaken = true;
-    }
-    public void addMilitaryToken(militaryToken m){
+    } // make our wonder taken
+    public void addMilitaryToken(militaryToken m){ // add our military token
         myMilitaryTokens.add(m);
     }
-    public void addAgeCard(ageCard card){
+    public void addAgeCard(ageCard card){ // we can add age card to list if we have enough resources
         if (card.requiredResources.equals(null) || this.IsEnoughResources(card.requiredResources))
             myCards.add(card);
         else return;
     }
     public void addTreasure(Treasure t){
         myTreasures.add(t);
-    }
-    ArrayList<ageCard> myCards = new ArrayList<ageCard>();
-    ArrayList<militaryToken> myMilitaryTokens = new ArrayList<militaryToken>();
-    ArrayList<Treasure> myTreasures = new ArrayList<Treasure>();
+    } //add treasure coin
+    ArrayList<ageCard> myCards = new ArrayList<ageCard>(); // all age cards of the wonder
+    ArrayList<militaryToken> myMilitaryTokens = new ArrayList<militaryToken>(); // all military token
+    ArrayList<Treasure> myTreasures = new ArrayList<Treasure>(); // all treasure coin
 
     public String getName() {
         return name;
@@ -59,7 +59,7 @@ public class Wonder {
     public MyStage getLocalStage() {
         return localStage;
     }
-    public int countMilitaryForce(){
+    public int countMilitaryForce(){ // here we count a number of red cards which we can use in war state
         int result  = 0;
         for (ageCard card : myCards){
             if (card.getCategory().equals("Red")){
@@ -71,7 +71,7 @@ public class Wonder {
         }
         return result;
     }
-    public void fight(int rightPlayer, int leftPlayer){
+    public void fight(int rightPlayer, int leftPlayer){ // here we compare our military forces with another players. And in case of stage or situation we will add military token
         militaryToken point5 = new militaryToken(5);
         militaryToken point3 = new militaryToken(3);
         militaryToken point1 = new militaryToken(1);
@@ -100,14 +100,14 @@ public class Wonder {
     public List<Resource> getResourceLocalStage(){
          return  localStage.getResources();
     }
-    public void nextStage(){
+    public void nextStage(){ // here we can move to the next stage
         if (localStage.equals(stage1))
             localStage = stage2;
         else if (localStage.equals(stage2))
             localStage = stage2;
         else return;
     }
-    public int countWinningPoint(){
+    public int countWinningPoint(){ // here we count all victory points of wonder what we have
         int result = 0;
         for (militaryToken mil : myMilitaryTokens) {
             result += (mil.getWinningPoints());
@@ -134,13 +134,13 @@ public class Wonder {
         }
         return result;
     }
-    public boolean IsEnoughResources(List<Resource> resourcesWeNeed){
+    public boolean IsEnoughResources(List<Resource> resourcesWeNeed){ // logic method is we need something more or not
         List<Resource> rest = this.resourcesNeed(resourcesWeNeed);
         if (rest.isEmpty())
             return true;
         else return false;
     }
-    public List<Resource> resourcesNeed(List<Resource> resourcesWeNeed){
+    public List<Resource> resourcesNeed(List<Resource> resourcesWeNeed){ //we create a copy of array what we have and what we need. After we delete all common from list what we need. And after we will return result
         ArrayList<Resource> copyAvailable = new ArrayList<Resource>();
         Collections.copy(availableResources, copyAvailable);
         resourcesWeNeed.removeAll(copyAvailable);
