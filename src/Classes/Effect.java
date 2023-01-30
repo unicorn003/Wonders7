8c9c28bf9ea9a341e84b9886fecde4782a6b2906
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:28:44 +0100

    Comments for Effect class

--- src/Classes/Effect.java
+++ src/Classes/Effect.java
@@ -1,17 +1,17 @@
 package Classes;
 
-public abstract class Effect {
+public abstract class Effect { //this class help us with counting point. class abstract, so we need to inherit another from it
     private String name;
     private String description;
     private int winningPoints;
 
-    public Effect(String n,String d, int w){
+    public Effect(String n,String d, int w){ //constructor
         name = n;
         description = d;
         winningPoints = w;
     }
 
-    public int getWinningPoints() {
+    public int getWinningPoints() { //return how many points we can get
         return winningPoints;
     }
 
