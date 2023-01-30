76b263622e832aaac1b1aeea5d80b577d0942372
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:41:07 +0100

    Comments for ageCard class

--- src/Classes/ageCard.java
+++ src/Classes/ageCard.java
@@ -2,15 +2,15 @@ package Classes;
 
 import java.util.List;
 
-public class ageCard {
-    private int age;
+public class ageCard { //main game card, we use it as a model
+    private int age; //in general, we have 3 stage, so 3 age
     private String name;
     private String description;
-    private String category;
-    private int minimumPlayers;
-    private boolean direction;
-    protected List<Resource> requiredResources;
-    protected Effect effects;
+    private String category; // color of card
+    private int minimumPlayers; // number of minimum players
+    private boolean direction; //direction can be right or left, so we can use true or false
+    protected List<Resource> requiredResources; // list of resources which we need to build this card
+    protected Effect effects; //how many winning point we can get from this card
 
     public ageCard(int a, String n, String d, String c, int mp, boolean dir, List<Resource> r,Effect ef){
         this.age = a;
