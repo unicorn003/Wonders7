e67f3a143e3fcf16e387501acce1824dd3bc15c8
unicorn03 <valentyna.pronina@nure.ua>
2023-01-30 23:25:35 +0100

    Comments for Game class

--- src/Classes/Game.java
+++ src/Classes/Game.java
@@ -3,10 +3,7 @@ package Classes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game {
-    List<Player> players = new ArrayList<Player>();
-    GUIParser myParser = new GUIParser();
-
-
-
+public class Game { //this  class represents game and will be implemented in future with another frontend representation
+    List<Player> players = new ArrayList<Player>(); //all players
+    GUIParser myParser = new GUIParser(); //frontend part
 }
